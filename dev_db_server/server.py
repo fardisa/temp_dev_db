a476490dd085c0786a422ddabed8126c9d340db7
Miracle Apata <prmpsmart@gmail.com>
2023-09-01 16:12:56 +0100

    misc

--- dev_db_server/server.py
+++ dev_db_server/server.py
@@ -1,23 +1,26 @@
 from typing import Union
 from fastapi import FastAPI
+from fastapi.openapi.utils import get_openapi
 from .users import *
 from .persist import *
 from .api_models import *
 
 
 dev_db_app = FastAPI(
+    title="dev_db: Temporary Dummy Database REST API Server",
+    version="1.0.0",
     on_startup=[load_t],
     on_shutdown=[Users.unwatch],
 )
 
 
 @dev_db_app.get("/create_user", status_code=200)
-def create_user(identifier: str) -> Union[CreateUserResponse, Response]:
+async def create_user(identifier: str) -> Union[CreateUserResponse, Response]:
     return Users.create_user(identifier)
 
 
 @dev_db_app.post("/delete_user", status_code=200)
-def delete_user(user_request: UserRequest) -> Response:
+async def delete_user(user_request: UserRequest) -> Response:
     return Users.delete_user(
         user_request.identifier,
         user_request.api_key,
@@ -25,20 +28,20 @@ def delete_user(user_request: UserRequest) -> Response:
 
 
 @dev_db_app.post("/get_user", status_code=200)
-def get_user(user_request: UserRequest) -> Union[GetUserResponse, Response]:
+async def get_user(user_request: UserRequest) -> Union[GetUserResponse, Response]:
     return Users.get_user(
         user_request.identifier,
         user_request.api_key,
     )
 
 
-@dev_db_app.get("/get_users", status_code=200)
-def get_users(identifier: str) -> Union[GetUsersResponse, Response]:
+@dev_db_app.get("/get_users", status_code=200, include_in_schema=False)
+async def get_users(identifier: str) -> Union[GetUsersResponse, Response]:
     return Users.get_users(identifier)
 
 
 @dev_db_app.post("/create_database", status_code=200)
-def create_database(
+async def create_database(
     create_database_request: CreateDatabaseRequest,
 ) -> Union[CreateDatabaseResponse, Response]:
     return Users.create_database(
@@ -50,7 +53,7 @@ def create_database(
 
 
 @dev_db_app.post("/delete_database", status_code=200)
-def delete_database(
+async def delete_database(
     delete_database_request: DeleteDatabaseRequest,
 ) -> Union[DeleteDatabaseResponse, Response]:
     return Users.delete_database(
@@ -61,7 +64,7 @@ def delete_database(
 
 
 @dev_db_app.post("/get_data", status_code=200)
-def get_data(
+async def get_data(
     data_request: DataRequest,
 ) -> Union[GetDataResponse, Response]:
     return Users.get_data(
@@ -72,7 +75,7 @@ def get_data(
 
 
 @dev_db_app.post("/set_data", status_code=200)
-def set_data(set_data_request: SetDataRequest):
+async def set_data(set_data_request: SetDataRequest):
     return Users.set_data(
         set_data_request.identifier,
         set_data_request.api_key,
@@ -82,7 +85,7 @@ def set_data(set_data_request: SetDataRequest):
 
 
 @dev_db_app.post("/delete_data", status_code=200)
-def delete_data(
+async def delete_data(
     data_request: DataRequest,
 ):
     return Users.delete_data(
